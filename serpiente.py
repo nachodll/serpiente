e1818deab228b3e7e524d98e77b6d9a97785941f
nachodll <nachodoradollamas@gmail.com>
2024-02-07 15:53:44 +0100

    better code

--- serpiente.py
+++ serpiente.py
@@ -28,13 +28,16 @@ class Serpiente:
     self.segmentos = []
     for posicion in POSICIONES_INICIALES:
       self.aniadir_segmento(posicion)
+
     self.cabeza = self.segmentos[0]
-    self.cabeza.shape(SPRITE_CABEZA_DERECHA)
-    self.segmentos[1].shape(SPRITE_TRONCO_HORIZONTAL)
-    self.segmentos[-1].shape(SPRITE_COLA_DERECHA)
+    self.cuello = self.segmentos[1]
+    self.cola = self.segmentos[-1]
+    self.actualizar_sprites()
 
   def extender(self):
     self.aniadir_segmento(self.segmentos[-1].position())
+    self.cola = self.segmentos[-1]
+    self.cola.hideturtle()
 
   def aniadir_segmento(self, posicion):
     nuevo_segmento = turtle.Turtle("square")
@@ -48,73 +51,79 @@ class Serpiente:
       self.segmentos[seg_num].goto(nueva_pos)
     self.cabeza.forward(SEGMENT_SIZE)
     self.actualizar_sprites()
+    self.cola.showturtle()
 
   def actualizar_sprites(self):
-    for seg_num in range(len(self.segmentos)-1, 0, -1):
-      # Primero despues de la cabeza
-      if seg_num == 1:
-        if (round(self.segmentos[seg_num].xcor()) == round(self.cabeza.xcor())
-            and round(self.segmentos[seg_num].xcor()) == round(self.segmentos[seg_num+1].xcor())):
-          self.segmentos[seg_num].shape(SPRITE_TRONCO_VERTICAL)
-        elif (round(self.segmentos[seg_num].ycor()) == round(self.cabeza.ycor())
-            and round(self.segmentos[seg_num].ycor()) == round(self.segmentos[seg_num+1].ycor())):
-          self.segmentos[seg_num].shape(SPRITE_TRONCO_HORIZONTAL)
-        elif round(self.segmentos[seg_num].xcor()) > round(self.cabeza.xcor()):
-          if round(self.segmentos[seg_num].ycor()) > round(self.segmentos[seg_num+1].ycor()):
-            self.segmentos[seg_num].shape(SPRITE_GIRO1)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_GIRO2)
-        elif round(self.segmentos[seg_num].xcor()) < round(self.cabeza.xcor()):
-          if round(self.segmentos[seg_num].ycor()) > round(self.segmentos[seg_num+1].ycor()):
-            self.segmentos[seg_num].shape(SPRITE_GIRO4)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_GIRO3)
-        elif round(self.segmentos[seg_num].ycor()) > round(self.cabeza.ycor()):
-          if round(self.segmentos[seg_num].xcor()) > round(self.segmentos[seg_num+1].xcor()):
-            self.segmentos[seg_num].shape(SPRITE_GIRO1)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_GIRO4)
-        elif round(self.segmentos[seg_num].ycor()) < round(self.cabeza.ycor()):
-          if round(self.segmentos[seg_num].xcor()) > round(self.segmentos[seg_num+1].xcor()):
-            self.segmentos[seg_num].shape(SPRITE_GIRO2)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_GIRO3)
+    # Cola
+    precola = self.segmentos[len(self.segmentos)-2]
+    if round(self.cola.xcor()) == round(precola.xcor()):
+      if self.cola.ycor() < precola.ycor():
+        self.cola.shape(SPRITE_COLA_ARRIBA)
+      else:
+        self.cola.shape(SPRITE_COLA_ABAJO)
+    else:
+      if round(self.cola.xcor()) < round(precola.xcor()):
+        self.cola.shape(SPRITE_COLA_DERECHA)
+      else:
+        self.cola.shape(SPRITE_COLA_IZQUIERDA)
 
-      # Cola
-      if seg_num == len(self.segmentos)-1:
-        if round(self.segmentos[seg_num].xcor()) == round(self.segmentos[seg_num-1].xcor()):
-          if self.segmentos[seg_num].ycor() < self.segmentos[seg_num-1].ycor():
-            self.segmentos[seg_num].shape(SPRITE_COLA_ARRIBA)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_COLA_ABAJO)
-        else:
-          if round(self.segmentos[seg_num].xcor()) < round(self.segmentos[seg_num-1].xcor()):
-            self.segmentos[seg_num].shape(SPRITE_COLA_DERECHA)
-          else:
-            self.segmentos[seg_num].shape(SPRITE_COLA_IZQUIERDA)
+    # Resto del cuerpo
+    for seg_num in range(len(self.segmentos)-2, 1, -1):
+      self.segmentos[seg_num].shape(self.segmentos[seg_num-1].shape())
 
-      # Resto del cuerpo
-      if seg_num != 1 and seg_num != len(self.segmentos)-1 and seg_num != 0:
-        self.segmentos[seg_num].shape(self.segmentos[seg_num-1].shape())
+    # Cabeza
+    if self.cabeza.heading() == ARRIBA:
+      self.cabeza.shape(SPRITE_CABEZA_ARRIBA)
+    elif self.cabeza.heading() == ABAJO:
+      self.cabeza.shape(SPRITE_CABEZA_ABAJO)
+    elif self.cabeza.heading() == DERECHA:
+      self.cabeza.shape(SPRITE_CABEZA_DERECHA)
+    elif self.cabeza.heading() == IZQUIERDA:
+      self.cabeza.shape(SPRITE_CABEZA_IZQUIERDA)
+
+    # Cuello
+    precuello = self.segmentos[2]
+    if (round(self.cuello.xcor()) == round(self.cabeza.xcor())
+        and round(self.cuello.xcor()) == round(precuello.xcor())):
+      self.cuello.shape(SPRITE_TRONCO_VERTICAL)
+    elif (round(self.cuello.ycor()) == round(self.cabeza.ycor())
+        and round(self.cuello.ycor()) == round(precuello.ycor())):
+      self.cuello.shape(SPRITE_TRONCO_HORIZONTAL)
+    elif round(self.cuello.xcor()) > round(self.cabeza.xcor()):
+      if round(self.cuello.ycor()) > round(precuello.ycor()):
+        self.cuello.shape(SPRITE_GIRO1)
+      else:
+        self.cuello.shape(SPRITE_GIRO2)
+    elif round(self.cuello.xcor()) < round(self.cabeza.xcor()):
+      if round(self.cuello.ycor()) > round(precuello.ycor()):
+        self.cuello.shape(SPRITE_GIRO4)
+      else:
+        self.cuello.shape(SPRITE_GIRO3)
+    elif round(self.cuello.ycor()) > round(self.cabeza.ycor()):
+      if round(self.cuello.xcor()) > round(precuello.xcor()):
+        self.cuello.shape(SPRITE_GIRO1)
+      else:
+        self.cuello.shape(SPRITE_GIRO4)
+    elif round(self.cuello.ycor()) < round(self.cabeza.ycor()):
+      if round(self.cuello.xcor()) > round(precuello.xcor()):
+        self.cuello.shape(SPRITE_GIRO2)
+      else:
+        self.cuello.shape(SPRITE_GIRO3)
 
           
   def derecha(self):
     if self.cabeza.heading() != IZQUIERDA:
       self.cabeza.setheading(DERECHA)
-      self.cabeza.shape(SPRITE_CABEZA_DERECHA)
 
   def arriba(self): 
     if self.cabeza.heading() != ABAJO:
       self.cabeza.setheading(ARRIBA)
-      self.cabeza.shape(SPRITE_CABEZA_ARRIBA)
 
   def izquierda(self):
     if self.cabeza.heading() != DERECHA:
       self.cabeza.setheading(IZQUIERDA)
-      self.cabeza.shape(SPRITE_CABEZA_IZQUIERDA)
 
   def abajo(self):
     if self.cabeza.heading() != ARRIBA:
       self.cabeza.setheading(ABAJO)
-      self.cabeza.shape(SPRITE_CABEZA_ABAJO)
       
